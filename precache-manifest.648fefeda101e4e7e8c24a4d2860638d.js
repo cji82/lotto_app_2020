c059957cb0255816bd5ede184e6f02bf5dc9c9cc
cji <ckwjddlf@naver.com>
2020-09-01 13:19:01 +0900

    Updates

---
+++ precache-manifest.648fefeda101e4e7e8c24a4d2860638d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1782b238f0779067695f17dba6c94b4",
+    "url": "/lotto_app_2020/index.html"
+  },
+  {
+    "revision": "b6dbc900ff4bb5cf7f26",
+    "url": "/lotto_app_2020/static/css/main.6b6b40d8.chunk.css"
+  },
+  {
+    "revision": "ed3797c9a4621cb487a5",
+    "url": "/lotto_app_2020/static/js/2.258c7420.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lotto_app_2020/static/js/2.258c7420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6dbc900ff4bb5cf7f26",
+    "url": "/lotto_app_2020/static/js/main.6f99f6eb.chunk.js"
+  },
+  {
+    "revision": "d781d4ea9b5677098aaf",
+    "url": "/lotto_app_2020/static/js/runtime-main.3eaefa46.js"
+  }
+]);
